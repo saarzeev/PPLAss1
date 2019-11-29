c07c44e077e91482af2df7f034ee3fe40e98c78e
saarzeev <saarzeev@post.bgu.ac.il>
2019-11-29 14:24:41 +0200

    Initial commit - Q1 is done

---
+++ ComplexNum.py
@@ -0,0 +1,72 @@
+import math
+
+class ComplexNum:
+    def __init__(self, re, im=0):
+        self.re = re
+        self.im = im
+
+        if im < 0:
+            self._im_sign = "-"
+        else:
+            self._im_sign = "+"
+
+    def re(self):
+        return self.re
+
+    def im(self):
+        return self.im
+
+    def to_tuple(self):
+        return (self.re, self.im)
+
+    def __repr__(self):
+        return str(self.re) + " " + self._im_sign + " " + str(abs(self.im)) + "i"
+
+    def __eq__(self, other):
+        if isinstance(other, (int, float)):
+            other = ComplexNum(other)
+
+        if isinstance(other, ComplexNum):
+            return self.im == other.im and self.re == other.re
+        else:
+            return False
+
+    def __add__(self, other):
+        if isinstance(other, (int, float)):
+            other = ComplexNum(other)
+
+        if isinstance(other, ComplexNum):
+            return ComplexNum(self.re + other.re, self.im + other.im)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __neg__(self):
+        return ComplexNum(- self.re, - self.im)
+
+    def __sub__(self, other):
+        if isinstance(other, (int, float)):
+            other = ComplexNum(other)
+
+        if isinstance(other, ComplexNum):
+            return self + (-other)
+
+    def __rsub__(self, other):
+        if isinstance(other, (int, float)):
+            other = ComplexNum(other)
+
+        if isinstance(other, ComplexNum):
+            return (-self) + other
+
+    def __mul__(self, other):
+        if not isinstance(other, ComplexNum):
+            raise TypeError("Complex multiplication only defined for Complex Numbers.")
+
+        # (x + yi)(u + vi) = (xu – yv) + (xv + yu)i
+        return ComplexNum(self.re * other.re() - self.im * other.im, self.re * other.im + self.im * other.re)
+
+    def conjugate(self):
+        return ComplexNum(self.re, -self.im)
+
+    def abs(self):
+        return math.sqrt(self*self.conjugate())
---
+++ main.py
@@ -0,0 +1,14 @@
+from ComplexNum import ComplexNum
+
+
+nu1 = ComplexNum(3)
+nu2 = ComplexNum(3, 0)
+
+print(nu1 + nu2)
+print(nu1 - nu2)
+print(nu1 - 3)
+print(3-nu1)
+print(-3+nu1)
+print(3+nu1)
+
+
